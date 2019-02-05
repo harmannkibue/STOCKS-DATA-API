a02d884909a8fbdfba365005b25ded684edb7322
harmannkibue <harmannkibue@gmail.com>
2019-02-05 11:21:30 +0300

    The stock API commit

---
+++ companies/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Stock
+
+class StockSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        # fields = ('ticker','volume')
+        fields ='__all__'
+
+
